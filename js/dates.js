5d79386ddf43576689e09fc39914af0eecdbd09e
brotherblazzard <blazzardj@byui.edu>
2025-05-02 12:44:57 -0600

    hello

--- js/dates.js
+++ js/dates.js
@@ -1,6 +1,12 @@
-let d = new Date(); // Create an instance of the date object with no parameter
-document.getElementById('lastmod').textContent = document.lastModified;
+// output elements
+const lastmod = document.getElementById('lastmod'); // last modified date
+const currentTerm = document.getElementById('currentTerm'); // current term
+const currentDate = document.getElementById('currentDate'); // current date and time
 
+
+let d = new Date(); // Create an instance of the date object with no parameter
+lastmod.textContent = `Last Update: ${ document.lastModified }`; // last modified date
+// get current term - BYU-Idaho
 let term = "";
 switch (d.getMonth()) {
   case 0:
@@ -32,4 +38,8 @@ document.getElementById("currentTerm").textContent = `${term} ${d.getFullYear()}
 const timezone = "America/Denver"; // Mountain Time Zone
 // const mountaintime = d.toLocaleTimeString('en-US', { timeZone: timezone });
 const mtntimeintl = d.toLocaleTimeString('en-GB', { timeZone: timezone });
-document.getElementById('currentDate').textContent = `Mountain Time ${mtntimeintl}`;
+currentDate.innerHTML = `Mountain Time: ${mtntimeintl}`;
+
+// get current UTC-0 time 
+const utc0time = d.toLocaleTimeString('en-GB', { timeZone: 'UTC' });
+currentDate.innerHTML += `<br>UTC-0: ${utc0time}`;
